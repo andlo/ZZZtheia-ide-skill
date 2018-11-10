2802501794188d7712a2369e094e918270434319
Andreas Lorensen <andlo@outlook.dk>
2018-11-10 13:49:33 +0000

    ...

--- files/run_theia.sh
+++ files/run_theia.sh
@@ -8,5 +8,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-pyls &
 yarn theia start /opt/mycroft/skills  --startup-timeout -1 --hostname 0.0.0.0 --port 3000
