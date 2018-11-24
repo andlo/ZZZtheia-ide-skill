075a564b14f7267e56d69c97cd048d56beb60a40
Andreas Lorensen <andlo@outlook.dk>
2018-11-24 13:23:29 +0000

    added voise to start and stop added better process handling added settings autos_tart

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import os
 import tarfile
 import wget
 import subprocess
+import signal
 
 
 class TheiaIde(MycroftSkill):
@@ -11,10 +12,37 @@ class TheiaIde(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
-        self.log.info("Initialize...")
-        SafePath = self.file_system.path
+        self.log.info("Initialize THEIA IDE...")
+        self.SafePath = self.file_system.path
+        self.theia_process = None
+        if not self.settings.get("theia installed") is True or self.settings.get("theia installed") is None:
+            self.install_theia()
+        if self.settings.get("auto_start") is True:
+            self.handle_ide_start()
+
+    @intent_file_handler('ide.theia.intent')
+    def handle_ide_theia(self, message):
+        self.speak_dialog('ide.theia')
+
+    @intent_file_handler('stop.intent')
+    def handle_ide_stop(self, message):
+        self.log.info("Stopping IDE")
+        os.killpg(self.theia_process.pid, signal.SIGKILL)
+        self.speak_dialog('IDE stopped')
+
+    @intent_file_handler('start.intent')
+    def handle_ide_start(self, message):
+        if self.theia_process is None:
+            self.log.info("Starting IDE")
+            self.theia_process = subprocess.Popen(self.SafePath + '/theia_run.sh ' +
+                                                  self.SafePath, preexec_fn=os.setsid, shell=True)
+            self.speak_dialog('IDE started')
+        else:
+            self.speak_dialog('IDE alreddy running')
+
+    def install_theia(self):
         platform = self.config_core.get('enclosure', {}).get('platform')
-        if not os.path.isfile(SafePath + '/theia_run.sh'):
+        if not os.path.isfile(self.SafePath + '/theia_run.sh'):
             self.speak_dialog('install_start')
             self.log.info(
                 "Downloading precompiled package for the " + platform + " platform.")
@@ -26,11 +54,12 @@ class TheiaIde(MycroftSkill):
             if platform == "mycroft_mark_1":
                 url = 'https://github.com/andlo/theia-for-mycroft/releases/download/THEIA-for-Mycroft/theiaide-mark1.tgz'
             try:
-                filename = wget.download(url, SafePath + '/theiaide.tgz')
-                self.speak_dialog('Unpacking...')
+                filename = wget.download(url, self.SafePath + '/theiaide.tgz')
+                self.speak_dialog('Unpacking theia IDE...')
                 package = tarfile.open(filename)
-                package.extractall(SafePath)
+                package.extractall(self.SafePath)
                 package.close()
+                os.remove(filename)
                 copyfile(self._dir + '/files/.editorconfig',
                          '/opt/mycroft/skills/.editorconfig')
                 self.log.info("Installed OK")
@@ -39,15 +68,6 @@ class TheiaIde(MycroftSkill):
             except Exception:
                 self.log.info("Theia not installed-something went wrong!")
                 self.speak_dialog('installed_BAD')
-        #if self.settings.get('theia installed') == 'True':
-        self.log.info("Starting THEIA IDE")
-        #os.system(SafePath + '/theia_run.sh ' + SafePath)
-        subprocess.call(SafePath + '/theia_run.sh ' + SafePath, shell=True)
-
-
-    @intent_file_handler('ide.theia.intent')
-    def handle_ide_theia(self, message):
-        self.speak_dialog('ide.theia')
 
 
 def create_skill():
